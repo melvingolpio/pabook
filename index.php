d315a30f8a8794dcd7b9d5606006a831e662f19c
melvingolpio <202101267@cityofmalabonuniversity.edu.ph>
2024-10-05 13:42:46 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,60 +1,71 @@
+<?php
+$host = 'us-cluster-east-01.k8s.cleardb.net';
+$user = 'b045ef9e80a154';
+$pass = 'de9cac97';
+$dbname = 'heroku_8c20245ae7e92fd';
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+// Retrieve slot statuses
+$sql = "SELECT slot_id, status FROM parking_slots";
+$result = $conn->query($sql);
+
+$slots = [];
+if ($result->num_rows > 0) {
+    while($row = $result->fetch_assoc()) {
+        $slots[$row["slot_id"]] = $row["status"];
+    }
+}
+
+$conn->close();
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Parking Management System</title>
+    <title>Real-Time Parking Status</title>
     <style>
         .slot {
             width: 100px;
             height: 100px;
+            border: 1px solid black;
             display: inline-block;
-            margin: 10px;
-            border: 2px solid black;
             text-align: center;
-            line-height: 100px;
+            vertical-align: middle;
             font-size: 20px;
-            font-weight: bold;
-            color: white;
         }
         .available { background-color: green; }
-        .reserved { background-color: yellow; color: black; }
         .occupied { background-color: red; }
     </style>
+
     <script>
-        // Function to refresh the parking slot status every 2 seconds
-        function refreshSlotStatus() {
-            var xhr = new XMLHttpRequest();
-            xhr.open('GET', 'get_parking_status.php', true);
-            xhr.onload = function () {
-                if (this.status == 200) {
-                    let slots = JSON.parse(this.responseText);
-                    for (let i = 0; i < 6; i++) {
-                        let slotElement = document.getElementById('slot' + (i+1));
-                        if (slots[i] === 'available') {
-                            slotElement.className = 'slot available';
-                        } else if (slots[i] === 'reserved') {
-                            slotElement.className = 'slot reserved';
-                        } else if (slots[i] === 'occupied') {
-                            slotElement.className = 'slot occupied';
-                        }
-                    }
+        function loadStatus() {
+            var xhttp = new XMLHttpRequest();
+            xhttp.onreadystatechange = function() {
+                if (this.readyState == 4 && this.status == 200) {
+                    document.getElementById("parkingSlots").innerHTML = this.responseText;
                 }
             };
-            xhr.send();
+            xhttp.open("GET", "fetch_slots.php", true);
+            xhttp.send();
         }
 
-        setInterval(refreshSlotStatus, 2000); // Refresh every 2 seconds
+        setInterval(loadStatus, 1000);  // Refresh every 3 seconds
     </script>
 </head>
-<body onload="refreshSlotStatus()">
+<body onload="loadStatus()">
     <h1>Parking Slot Status</h1>
-    <div id="slot1" class="slot">1</div>
-    <div id="slot2" class="slot">2</div>
-    <div id="slot3" class="slot">3</div>
-    <div id="slot4" class="slot">4</div>
-    <div id="slot5" class="slot">5</div>
-    <div id="slot6" class="slot">6</div>
+    <div id="parkingSlots">
+        <!-- Slots will be loaded here -->
+    </div>
 </body>
 </html>
 
